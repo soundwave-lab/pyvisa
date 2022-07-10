8ce26eacf0d1059b6345e538b1828a7f1d036ffb
efficient-life <efficient122@gmail.com>
2022-07-10 22:23:22 +0900

    first commit

---
+++ device/functionGenerater.py
@@ -0,0 +1,13 @@
+# APPLy?
+# "SIN +5.0000000000000E+03,+3.0000000000000E+00,-2.5000000000000E+00"
+
+import pyvisa
+
+
+class FunctionGenerater:
+    def __init__(self):  # 接続先を指定
+        print('sdfa')
+
+    @classmethod
+    def test(cls):
+        print('hello world')
